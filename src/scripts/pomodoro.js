786cf2cb97c50180de12caefdb6fd1df7fc0c5a2
arcanjo17 <gabriel.arcanjo.1705@outlook.com>
2026-07-28 16:37:59 -0300

    iniciando o cronometro

--- src/scripts/pomodoro.js
+++ src/scripts/pomodoro.js
@@ -1,8 +1,27 @@
+import { resetTimer } from "./timer.js"
+
 const tabs = document.querySelectorAll('.timer-card__tab')
 const page = document.querySelector('.page')
 const modeImage = document.querySelector('.banner img')
+const bannerTitleText = document.querySelector('.banner__title--text')
+const bannerTitleHighlight = document.querySelector('.banner__title--highlight')
 
 let mode = 1
+
+const context = {
+    '1': {
+        title: 'Otimize sua produtividade,',
+        highlight: 'mergulhe no que importa'
+    }, 
+    '2': {
+        title: 'Que tal dar uma respirada?',
+        highlight: 'Faça uma pausa curta!'
+    }, 
+    '3': {
+        title: 'Hora de voltar à superfície.',
+        highlight: 'Faça uma pausa longa.'
+    }
+}
 
 tabs.forEach(tab => {
     tab.addEventListener('click', event => {
@@ -19,6 +38,12 @@ tabs.forEach(tab => {
 
         const modeImageSrc = `/mode-${mode}.png`
         modeImage.setAttribute('src', modeImageSrc)
+
+        const modeContext = context[mode]
+        bannerTitleText.textContent = modeContext.title
+        bannerTitleHighlight.textContent = modeContext.highlight
+
+        resetTimer(mode)
         
         event.currentTarget.disabled = true
         event.currentTarget.classList.add('timer-card__tab--active')
